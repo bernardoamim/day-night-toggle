8677d9c26d5c32dbd46c4de5a8e0e252f400bd3b
bernardo.amim <bernardo.amim@mercatus.com>
2023-05-16 17:09:14 -0300

    finishing animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 import CloudSVG from '../public/assets/svg/cloud.svg'
 import StarsSVG from '../public/assets/svg/stars.svg'
-// import MoonSVG from '../public/assets/svg/moon.svg'
+import MoonHolesSVG from '../public/assets/svg/moon-holes.svg'
 import Plane from '../public/assets/img/plane.png'
 
 import SunSVG from '../components/SunSVG'
@@ -17,13 +17,18 @@ const pillAnimatedClasses = {
 }
 
 const sunAnimatedClasses = {
-  light: `opacity-100 duration-700`,
-  dark: `opacity-0 duration-700 dark:hidden`,
+  light: `opacity-100 translate-x-0 duration-700 `,
+  dark: `opacity-0 translate-x-20 duration-700`,
 }
 
 const moonAnimatedClasses = {
-  light: `opacity-0 duration-700`,
-  dark: `opacity-100 duration-700`,
+  light: `opacity-0 -translate-x-20 duration-700`,
+  dark: `opacity-100 translate-x-0 duration-700`,
+}
+
+const moonHolesAnimatedClasses = {
+  light: `opacity-0 -translate-x-20 duration-700`,
+  dark: `opacity-100 translate-x-0 duration-700`,
 }
 
 const cloudAnimatedClasses = {
@@ -42,7 +47,7 @@ const starsAnimatedClasses = {
 }
 
 export default function Home() {
-  const [theme, toggleTheme] = useTheme('dark')
+  const [theme, toggleTheme] = useTheme('light')
 
   return (
     <main className="flex min-h-screen max-w-screen flex-col items-center justify-center p-24">
@@ -52,48 +57,43 @@ export default function Home() {
       <h2 className="text-2xl mb-20">
         Yes, it&apos;s the same one that made us all laugh on TikTok... 😅
       </h2>
-      <div className="flex gap-4">
-        <div className="flex flex-col justify-between h-48 w-48 p-6 border border-dashed border-[#9747FF] rounded-md">
-          <div
-            className={`w-full rounded-full h-16 relative ${pillAnimatedClasses[theme]} overflow-clip`}
-            onClick={toggleTheme}
-          >
-            {/** Day Elements */}
-            <SunSVG
-              className={`absolute left-[-3rem] bottom-[-2.65rem] transition-opacity ease-out ${sunAnimatedClasses[theme]}`}
-            />
-            <Image
-              src={CloudSVG}
-              alt="Cloud svg"
-              className={`absolute right-0 bottom-0 transition-all ease-out ${cloudAnimatedClasses[theme]}`}
-            />
-            <Image
-              src={Plane}
-              alt="Plane illustration"
-              className={`absolute right-0 bottom-0 z-10 w-14 transition-all ease-out ${planeAnimatedClasses[theme]}`}
-            />
 
-            {/** Night Elements */}
-
-            <Image
-              src={StarsSVG}
-              alt="Cloud svg"
-              className={`absolute left-4 bottom-2 z-10 transition-all ease-out ${starsAnimatedClasses[theme]}`}
-            />
-
-            {/* <Image
-              src={MoonSVG}
-              alt="Cloud svg"
-              className={`absolute right-[-2rem] bottom-[-2rem] transition-all ease-out ${moonAnimatedClasses[theme]}`}
-            /> */}
-
-            <MoonSVG
-              className={`absolute left-[-0.3rem] top-[-2.7rem] transition-opacity ease-out ${moonAnimatedClasses[theme]}`}
-            />
-          </div>
+      <div className="flex flex-col justify-center h-48 w-48 p-6 border border-dashed border-[#9747FF] rounded-md">
+        <div
+          className={`w-full rounded-full h-16 relative ${pillAnimatedClasses[theme]} overflow-clip`}
+          onClick={toggleTheme}
+        >
+          {/** Day Elements */}
+          <SunSVG
+            className={`absolute left-[-3rem] bottom-[-2.65rem] transition-all ease-in-out ${sunAnimatedClasses[theme]}`}
+          />
+          <Image
+            src={CloudSVG}
+            alt="Cloud svg"
+            className={`absolute right-0 bottom-0 transition-all ease-in-out ${cloudAnimatedClasses[theme]}`}
+          />
+          <Image
+            src={Plane}
+            alt="Plane illustration"
+            className={`absolute right-0 bottom-0 z-10 w-14 transition-all ease-in-out ${planeAnimatedClasses[theme]}`}
+          />
 
           {/** Night Elements */}
-          <div className="bg-[#151535] w-full rounded-full h-16"></div>
+
+          <Image
+            src={StarsSVG}
+            alt="Cloud svg"
+            className={`absolute left-4 bottom-2 z-10 transition-all ease-in-out ${starsAnimatedClasses[theme]}`}
+          />
+
+          <MoonSVG
+            className={`absolute left-[-0.3rem] top-[-2.7rem] transition-all ease-in-out ${moonAnimatedClasses[theme]}`}
+          />
+          <Image
+            src={MoonHolesSVG}
+            alt="Moon Holes svg"
+            className={`absolute right-[1.3rem] bottom-[1rem] transition-all ease-in-out ${moonHolesAnimatedClasses[theme]}`}
+          />
         </div>
       </div>
     </main>
